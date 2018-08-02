22f6d7b7e0e8b5537063c72b8a6c1f68dc4d6a8d
syauqi <syauqilenterano@gmail.com>
2018-08-02 10:37:50 +0700

    please

--- C++Programs/source.main/wavesim.cc
+++ C++Programs/source.main/wavesim.cc
@@ -311,7 +311,7 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 			u[i+1][j]=Up[pos];
 		}
 	}
-	#pragma acc exit data delete(U[:5][:Nx*Ny],Ux,Uy,left_sstep,right_sstep,bottom_sstep,tstep,left_cfabc,right_cfabc,bottom_cfabc,Up,Uo,Um,Uxo,Uxm,Uxp,Uyo,Uyp,Uym,u)
+	#pragma acc exit data delete(U[:5][:Nx*Ny],Ux[:5][:Nx*Ny],Uy[:5][:Nx*Ny],left_sstep[:81],right_sstep[:81],bottom_sstep[:81],tstep[:81],left_cfabc[:ny][:81],right_cfabc[:ny][:81],bottom_cfabc[:ny][:81],Up[:Nx*Ny],Uo[:Nx*Ny],Um[:Nx*Ny],Uxo[:Nx*Ny],Uxm[:Nx*Ny],Uxp[:Nx*Ny],Uyo[:Nx*Ny],Uyp[:Nx*Ny],Uym[:Nx*Ny],u[:nx*ny])
 	free_mat_mem(U); free_mat_mem(Ux); free_mat_mem(Uy);
 	free_mat_mem(left_cfabc); free_mat_mem(right_cfabc); free_mat_mem(bottom_cfabc);
 	delete [] left_sstep; delete [] right_sstep; delete [] bottom_sstep; delete [] tstep;
