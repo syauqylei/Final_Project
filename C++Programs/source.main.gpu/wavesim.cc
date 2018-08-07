17edb2e287d00de5177c72f3732b576d4aa4955d
syauqi <syauqilenterano@gmail.com>
2018-08-07 19:21:43 +0700

    heh

--- C++Programs/source.main.gpu/wavesim.cc
+++ C++Programs/source.main.gpu/wavesim.cc
@@ -263,6 +263,7 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 				Uy[4][Ny*Nx-Nx+j]=Uybdrbottom;
 		}
 		
+		std::cout<<i<<"\n";
 		#pragma acc parallel loop 
 		for (int j=0;j<nx*ny;j++)	
 		{
@@ -284,12 +285,14 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 			Uy[3][pos]=Uy[4][pos];
 		}
 		
+		std::cout<<i<<"\n";
 		#pragma acc kernels copyout(u[i:i+1][0:nx])
 		for (int j=0;j<nx;j++)
 		{
 			int pos=stencil[j];
 			u[i+1][j]=U[4][pos];
 		}
+		
 	}
 	#pragma acc exit data delete(U[:5][:Nx*Ny],Ux[:5][:Nx*Ny],Uy[:5][:Nx*Ny])
 	#pragma acc exit data delete(left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81])
