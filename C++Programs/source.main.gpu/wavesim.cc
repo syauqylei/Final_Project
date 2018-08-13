cdd72677109ba9bc6b4952a7d4cb5a79c525dbbf
syauqi <syauqilenterano@gmail.com>
2018-08-13 10:28:29 +0700

    odnt

--- C++Programs/source.main.gpu/wavesim.cc
+++ C++Programs/source.main.gpu/wavesim.cc
@@ -104,8 +104,9 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 		{
 		int source_loc=stencil[srcloc];
 		U[3][source_loc]=-5.76*freq*freq*(1-16.0*(0.6*freq*t-1)*(0.6*freq*t-1)) *exp(-8.0* (0.6*freq*t-1)*(0.6*freq*t-1));
-		}
 		std::cout<<U[3][source_loc]<<"\n";
+		}
+		
 		//Calculate Wavefield
 		#pragma acc parallel loop
 		for (int j=0; j<nx*ny;j++){
