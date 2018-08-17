6173a90fcd1ad3bef92bc84196cac386887ff4f6
syauqi <syauqilenterano@gmail.com>
2018-08-17 22:52:26 +0700

    fukcin joke

--- C++Programs/source.main.multgpus/rnw.h
+++
@@ -1,8 +0,0 @@
-#ifndef RNW
-#define RNW
-void read_vel(const std::string& filename, std::vector <double>& vel , int &nx,int &ny, int &nz, double &h);
-void w_dat(const std::string& filename, double *Vel_mod, double **P,double dt,double h,int nt,int nx, int ny, int nz,double a,double b, double c);
-void read_fwdset(const std::string& filename, std::vector <int>& srcloc,int &ns, double &dt,double &T,double &fm);
-void write_wve_txt(const std::string& filename, double **U , int nx,int ny, int nt);
-void write_rec_txt(const std::string& filename, double **U,int xsource,int ysource,double h,double dt , int nx,int ny, int nt);
-#endif
