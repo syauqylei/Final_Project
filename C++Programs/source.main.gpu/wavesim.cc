159cc5a0c5c6d27a6c67810e4c0549a5240ed723
syauqi <syauqilenterano@gmail.com>
2018-08-18 17:21:48 +0700

    try to implement modular progarming in openacc

--- C++Programs/source.main.gpu/wavesim.cc
+++ C++Programs/source.main.gpu/wavesim.cc
@@ -84,6 +84,7 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 		gen_cfabc(bottom_cfabc[i],vel[nx*ny-nx+i],dt,h,beta);
 		}
 	double t;
+	#pragma acc data copyout(u[0:nt][0:nx])
 	for (int i=0; i<nt-1;i++)
 	{
 		//time step./w	
@@ -91,7 +92,7 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 		if((i)%(nt/10)==0){
 		std::cout<<std::fixed<<std::setprecision(1)<<"Calculating Wavefield ... "<<float(i)/float(nt)*100.0<<"%\n";}
 		
-		#pragma acc data copyin(U[0:5][0:Nx*Ny],Ux[0:5][0:Nx*Ny],Uy[0:5][0:Nx*Ny]) copyout(u[0:nt][0:nx]) present(stencil[0:nx*ny],vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81],left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
+		#pragma acc data copyin(U[0:5][0:Nx*Ny],Ux[0:5][0:Nx*Ny],Uy[0:5][0:Nx*Ny]) present(stencil[0:nx*ny],vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81],left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
 		{
 		//Top neumann boundary
 		#pragma acc parallel loop
