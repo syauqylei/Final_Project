19dc54f470988b0844aa138135212d689e3070d0
syauqi <syauqilenterano@gmail.com>
2018-08-18 16:09:45 +0700

    try to implement modular progarming in openacc

--- C++Programs/source.main.gpu/abcon.h
+++ C++Programs/source.main.gpu/abcon.h
@@ -1,10 +1,14 @@
 #ifndef ABC
 #define ABC
 
-#pragma acc routine seq {extern void gen_sstep(int *s_step,int pole);}
+#pragma acc routine seq
+extern void gen_sstep(int *s_step,int pole);
 
-#pragma acc routine seq {extern void gen_tstep(int *t_step);}
-#pragma acc routine seq {extern void gen_cfabc(double *cfabc,double c,double dt,double h,double *beta);}
+#pragma acc routine seq
+extern void gen_tstep(int *t_step);
+
+#pragma acc routine seq
+extern void gen_cfabc(double *cfabc,double c,double dt,double h,double *beta);
 /*
 #pragma acc routine gang
 extern double habc(double **U, double *cfabc, int *tstep, int *sstep, int pos);
