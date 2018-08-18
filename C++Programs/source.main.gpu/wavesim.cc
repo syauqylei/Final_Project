ffa6ceb632b7a90ae2600adc9f10f75d29fa46a4
syauqi <syauqilenterano@gmail.com>
2018-08-18 12:42:05 +0700

    lul

--- C++Programs/source.main.gpu/wavesim.cc
+++ C++Programs/source.main.gpu/wavesim.cc
@@ -69,13 +69,15 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 	double **__restrict__ bottom_cfabc=alloc_mat(nx,81);
 	
 	#pragma acc enter data create(left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81])
-
+	
+	#pragma acc kernels
 	for (int i=0;i<ny;i++)
 		{	
 		gen_cfabc(left_cfabc[i],vel[i*nx],dt,h,beta);
 		gen_cfabc(right_cfabc[i],vel[(i+1)*nx-1],dt,h,beta);
 		}
 
+	#pragma acc kernels
 	for (int i=0;i<nx;i++)
 		{
 		gen_cfabc(bottom_cfabc[i],vel[nx*ny-nx+i],dt,h,beta);
@@ -283,7 +285,7 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 			Uy[3][pos]=Uy[4][pos];
 		}
 		
-		#pragma acc parallel loop 
+		#pragma acc parallel loop
 		for (int j=0;j<nx;j++)
 		{
 			int pos=stencil[j];
