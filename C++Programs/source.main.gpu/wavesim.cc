028122a3a648e611d6ab17e779a74fdefc639ddf
syauqi <syauqilenterano@gmail.com>
2018-08-18 21:49:34 +0700

    lul

--- C++Programs/source.main.gpu/wavesim.cc
+++ C++Programs/source.main.gpu/wavesim.cc
@@ -11,7 +11,6 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 	int Nx=nx+2;
 	int Ny=ny+2;
 	
-	#pragma acc enter data copyin(vel[0:nx*ny])
 	//Alloc array to store wavefield
 	double **__restrict__ u=alloc_mat(nt,nx);
 	double **__restrict__ U=alloc_mat(5,Nx*Ny);
@@ -40,52 +39,46 @@ double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, d
 	
 	//create stencil
 	int *__restrict__ stencil= new int[nx*ny];
-	#pragma acc enter data create(stencil[0:nx*ny])
 	
+	#pragma acc parallel loop copyin(stencil[0:nx*ny])
 	for (int i=1;i<Ny-1;i++){
 		for (int j=1;j<Nx-1;j++){
 			stencil[(i-1)*nx+j-1]=i*Nx+j;
 			}
 		}
 
-	#pragma acc update device(stencil[0:nx*ny])
 	//allocate array spatial operator for ABC
 	int *__restrict__ left_sstep=new int[81];
 	int *__restrict__ right_sstep=new int[81];
 	int *__restrict__ bottom_sstep=new int[81];
 	int *__restrict__ tstep=new int[81];
 
-	#pragma acc enter data create(left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
 	//fill ABC operator
 	gen_sstep(left_sstep,1);
 	gen_sstep(right_sstep,-1);
 	gen_sstep(bottom_sstep,-Nx);
 	gen_tstep(tstep);
 
-	#pragma acc update device(left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
-	
 	//allocate array for coefficient of ABC hihgdon
 	double **__restrict__ left_cfabc=alloc_mat(ny,81);
 	double **__restrict__ right_cfabc=alloc_mat(ny,81);
 	double **__restrict__ bottom_cfabc=alloc_mat(nx,81);
 	
-	#pragma acc enter data create(left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81])
-	
-	#pragma acc kernels copyin(vel[0:nx*ny]) present(left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81])
+	#pragma acc kernels copyin(vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81])
 	for (int i=0;i<ny;i++)
 		{
 		gen_cfabc(left_cfabc[i],vel[i*nx],dt,h,beta);
 		gen_cfabc(right_cfabc[i],vel[(i+1)*nx-1],dt,h,beta);
 		}
 
-	#pragma acc kernels copyin(vel[0:nx*ny]) present(bottom_cfabc[0:nx][0:81])
+	#pragma acc kernels copyin(vel[0:nx*ny],bottom_cfabc[0:nx][0:81])
 	for (int i=0;i<nx;i++)
 		{
 		gen_cfabc(bottom_cfabc[i],vel[nx*ny-nx+i],dt,h,beta);
 		}
 	double t;
 	
-	#pragma acc data copyin(U[0:5][0:Nx*Ny],Ux[0:5][0:Nx*Ny],Uy[0:5][0:Nx*Ny]) copyout(u[0:nt][0:nx]) present(stencil[0:nx*ny],vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81],left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
+	#pragma acc data copyin(U[0:5][0:Nx*Ny],Ux[0:5][0:Nx*Ny],Uy[0:5][0:Nx*Ny],stencil[0:nx*ny],vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81],left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81]) copyout(u[0:nt][0:nx]) 
 	for (int i=0; i<nt-1;i++)
 	{
 		//time step./w	
