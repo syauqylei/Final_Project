81c4728380f3c0e0943d04de1cf9cd34bb599612
syauqi <syauqilenterano@gmail.com>
2018-08-30 07:57:53 +0700

    idk

--- C++Programs/source.main.multgpu/arrayman.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARRAYMAN_H
-#define ARRAYMAN_H
-double **alloc_mat(const int nrows, const int ncols);
-void free_mat_mem(double **mat);
-#endif
-
---
+++ C++Programs/source.rtm/arrayman.h
@@ -0,0 +1,6 @@
+#ifndef ARRAYMAN_H
+#define ARRAYMAN_H
+double **alloc_mat(const int nrows, const int ncols);
+void free_mat_mem(double **mat);
+#endif
+
