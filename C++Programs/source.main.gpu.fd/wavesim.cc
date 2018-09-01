35fe319c186d356bd46820f76fc1eee4da2ba34e
syauqi <syauqilenterano@gmail.com>
2018-09-01 10:04:45 +0700

    idk

---
+++ C++Programs/source.main.gpu.fd/wavesim.cc
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <iomanip>
+#include <accelmath.h>
+#include "arrayman.h"
+#include "hider.h"
+#include "abcon.h"
+#include "openacc.h"
+
+double **wvenacd(double *vel, int nx, int ny,int srcloc, double freq,double h, double dt,double T){
+	int nt=int(T/dt);
+	int Nx=nx+4;
+	int Ny=ny+4;
+	
+	//Alloc array to store wavefield
+	double **__restrict__ u=alloc_mat(nt,nx*ny);
+	
+	double **__restrict__ U=alloc_mat(5,Nx*Ny);
+	
+	//create data on device
+	#pragma acc kernels copyout(U[:5][:Nx*Ny])
+	for (int i=0;i<5;i++)
+	{
+		for (int j=0;j<Nx*Ny;j++)
+		{
+			U[i][j]=0.0;
+		}
+	}
+	
+	//create angle factor for abc higdon
+	double *__restrict__ beta= new double[4];
+	beta[0]=0.99;
+	beta[1]=0.79;
+	beta[2]=0.52;
+	beta[3]=0.31;
+	
+	//create stencil
+	int *__restrict__ stencil= new int[nx*ny];
+	
+	#pragma acc parallel loop copyin(stencil[0:nx*ny])
+	for (int i=2;i<Ny-2;i++){
+		for (int j=2;j<Nx-2;j++){
+			stencil[(i-2)*nx+j-2]=i*Nx+j;
+			}
+		}
+
+	//allocate array spatial operator for ABC
+	int *__restrict__ left_sstep=new int[81];
+	int *__restrict__ right_sstep=new int[81];
+	int *__restrict__ bottom_sstep=new int[81];
+	int *__restrict__ tstep=new int[81];
+
+	//fill ABC operator
+	gen_sstep(left_sstep,1);
+	gen_sstep(right_sstep,-1);
+	gen_sstep(bottom_sstep,-Nx);
+	gen_tstep(tstep);
+
+	//allocate array for coefficient of ABC hihgdon
+	double **__restrict__ left_cfabc=alloc_mat(ny,81);
+	double **__restrict__ right_cfabc=alloc_mat(ny,81);
+	double **__restrict__ bottom_cfabc=alloc_mat(nx,81);
+	
+	#pragma acc kernels copyin(vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81])
+	for (int i=0;i<ny;i++)
+		{
+		gen_cfabc(left_cfabc[i],vel[i*nx],dt,h,beta);
+		gen_cfabc(right_cfabc[i],vel[(i+1)*nx-1],dt,h,beta);
+		}
+
+	#pragma acc kernels copyin(vel[0:nx*ny],bottom_cfabc[0:nx][0:81])
+	for (int i=0;i<nx;i++)
+		{
+		gen_cfabc(bottom_cfabc[i],vel[nx*ny-nx+i],dt,h,beta);
+		}
+	double t;
+	
+	#pragma acc enter data copyin(U[0:5][0:Nx*Ny],stencil[0:nx*ny],vel[0:nx*ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81],left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
+	
+	#pragma acc data copyout(u[0:nt][0:nx]) 
+	for (int i=0; i<nt-1;i++)
+	{
+		//time step./w	
+		t=dt*(i);
+		if((i)%(nt/10)==0){
+		std::cout<<std::fixed<<std::setprecision(1)<<"Calculating Wavefield ... "<<float(i)/float(nt)*100.0<<"%\n";}
+		
+		//Top neumann boundary
+		#pragma acc parallel loop present(U[0:5][0:Nx*Ny])
+		for (int j=0;j<Nx;j++)
+		{
+			U[3][j]=U[3][j+3*Nx];
+			U[3][j+Nx]=U[3][j+2*Nx];
+		}
+		
+		#pragma acc kernels present(U[0:5][0:Nx*Ny])
+		{
+		int source_loc=stencil[srcloc];		
+		U[3][source_loc]=-5.76*freq*freq*(1-16.0*(0.6*freq*t-1)*(0.6*freq*t-1)) *exp(-8.0* (0.6*freq*t-1)*(0.6*freq*t-1));
+		}
+		
+		//Calculate Wavefield
+		#pragma acc parallel loop present(U[0:5][0:Nx*Ny],stencil[0:nx*ny],vel[0:nx*ny])
+		for (int j=0; j<nx*ny;j++){
+			int pos=stencil[j];
+			double cf1;
+			cf1=vel[j]*vel[j]*dt*dt;
+			U[4][pos]=2.0*U[3][pos]-U[2][pos]+cf1*(-60.0*U[3][pos]
+													+16.0*U[3][pos+1]+16.0*U[3][pos-1]
+													+16.0*U[3][pos+Nx]+16.0*U[3][pos-Nx]
+													+U[3][pos+2]+U[3][pos-2]
+													+U[3][pos+2*Nx]+U[3][pos-2*Nx]);
+			}
+		
+		//calculate ABC higdon boundary
+		#pragma acc parallel loop present(U[0:5][0:Nx*Ny],left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],left_sstep[0:81],right_sstep[0:81],tstep[0:81])
+		for (int j=2;j<Ny-2;j++) 
+		{	
+			U[4][j*Nx+2]=habc(U,left_cfabc[j-2],tstep,left_sstep,j*Nx+2);
+			U[4][(j+1)*Nx-3]=habc(U,right_cfabc[j-2],tstep,right_sstep,(j+1)*Nx-3);
+		}
+		
+		#pragma acc parallel loop present(U[0:5][0:Nx*Ny],tstep[0:81],bottom_cfabc[0:nx][0:81],bottom_sstep[0:81],tstep[0:81])
+		for (int j=2;j<Nx-2;j++)
+		{
+			U[4][Ny*Nx-2*Nx+j]=habc(U,bottom_cfabc[j-2],tstep,bottom_sstep,Ny*Nx-2*Nx+j);
+		}
+		
+		#pragma acc parallel loop present(U[0:5][0:Nx*Ny],stencil[0:nx*ny])
+		for (int j=0;j<nx*ny;j++)	
+		{
+			int pos=stencil[j];
+			U[0][pos]=U[1][pos];
+			U[1][pos]=U[2][pos];
+			U[2][pos]=U[3][pos];
+			U[3][pos]=U[4][pos];
+		}
+		
+		#pragma acc parallel loop
+		for (int j=0;j<nx*ny;j++)
+		{
+			int pos=stencil[j];
+			u[i+1][j]=U[4][pos];
+		}
+	}
+	
+	#pragma acc exit data delete(U[0:5][0:Nx*Ny])
+	#pragma acc exit data delete(left_cfabc[0:ny][0:81],right_cfabc[0:ny][0:81],bottom_cfabc[0:nx][0:81])
+	#pragma acc exit data delete(left_sstep[0:81],right_sstep[0:81],bottom_sstep[0:81],tstep[0:81])
+	#pragma acc exit data delete(vel[0:nx*ny],stencil[0:nx*ny])
+	
+	free_mat_mem(U);
+	free_mat_mem(left_cfabc); free_mat_mem(right_cfabc); free_mat_mem(bottom_cfabc);
+	delete [] left_sstep; delete [] right_sstep; delete [] bottom_sstep; delete [] tstep;
+	return u;}
