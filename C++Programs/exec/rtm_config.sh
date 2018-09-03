3f9e4f1ee01145936d024e6b0ccd7d815286e2b4
Syauqy_Guriang <syauqi@guriang.unpad.ac.id>
2018-09-03 21:28:08 +0700

    il

--- C++Programs/exec/rtm_config.sh
+++ C++Programs/exec/rtm_config.sh
@@ -2,9 +2,9 @@
 
 for i in {20..440..20}
 do
-	par1="./images/FD/image$i"
+	par1="./images/FD4/image$i"
 	par2="./marm.txt"
 	par3="./setfile$i.txt"
 	par4="./rec$i.txt"
-	./Rtm_fd $par1 $par2 $par3 $par4
+	./Rtm_fd4 $par1 $par2 $par3 $par4
 done
