2e241a8c6d6bcec72f7f7c00dd3f6e434ccec6a7
syauqi <syauqilenterano@gmail.com>
2018-09-03 21:07:43 +0700

    idk

--- C++Programs/exec/images/FD/imagefilter.py
+++ C++Programs/exec/images/FD/imagefilter.py
@@ -22,9 +22,32 @@ for i in range(20,440,20):
 	plt.imshow(data,cmap="gray",extent=[0,9,3.5,0])
 	plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
 	plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
-	plt.savefig("images/image"+str(i)+".eps",bbox_inches='tight')
+	plt.savefig("image"+str(i)+".eps",bbox_inches='tight')
 	
 	plt.imshow(highpass_5x5,cmap="gray",extent=[0,9,3.5,0])
 	plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
 	plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
-	plt.savefig("images/imagefiltered"+str(i)+".eps",bbox_inches='tight')
+	plt.savefig("imagefiltered"+str(i)+".eps",bbox_inches='tight')
+
+im = np.genfromtxt("image.txt")
+data = np.array(im, dtype=float)
+
+kernel = np.array([[-1, -1, -1, -1, -1],
+					   [-1,  1,  2,  1, -1],
+					   [-1,  2,  4,  2, -1],
+					   [-1,  1,  2,  1, -1],
+					   [-1, -1, -1, -1, -1]])
+
+highpass_5x5 = ndimage.convolve(data, kernel)
+
+np.savetxt("imagefiltered.txt",highpass_5x5)
+
+plt.imshow(data,cmap="gray",extent=[0,9,3.5,0])
+plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
+plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
+plt.savefig("image.eps",bbox_inches='tight')
+
+plt.imshow(highpass_5x5,cmap="gray",extent=[0,9,3.5,0])
+plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
+plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%d km'))
+plt.savefig("imagefiltered.eps",bbox_inches='tight')
