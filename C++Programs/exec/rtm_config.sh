149a3e13c7e6ecb3ac8ccfa0946d43f0dd513967
Syauqy_Guriang <syauqi@guriang.unpad.ac.id>
2018-09-13 22:12:14 +0700

    lul

--- C++Programs/exec/rtm_config.sh
+++ C++Programs/exec/rtm_config.sh
@@ -2,8 +2,8 @@
 
 for i in {20..440..20}
 do
-	par1="./images/FD4/image$i"
-	par2="./marm.txt"
+	par1="./scratch/syauqy/result/NACD/image$i"
+	par2="./velbp2004.txt"
 	par3="./setfile$i.txt"
 	par4="./rec$i.txt"
 	./Rtm_fd4 $par1 $par2 $par3 $par4
