c0aded175b187031d535b3be3fc9d0e786899ede
Syauqy_Guriang <syauqi@guriang.unpad.ac.id>
2018-09-15 08:00:38 +0700

    idk

--- C++Programs/exec/rtm_config.sh
+++ C++Programs/exec/rtm_config.sh
@@ -5,6 +5,6 @@ do
 	par1="/scratch/syauqy/result/NACD/image$i"
 	par2="./velbp2004new.txt"
 	par3="./setfile$i.txt"
-	par4="./rec$i.txt"
-	./Rtm_fd4 $par1 $par2 $par3 $par4
+	par4="/scratch/syauqy/record/SynBP2004/rec$i.txt"
+	./rtm_nacd $par1 $par2 $par3 $par4
 done
