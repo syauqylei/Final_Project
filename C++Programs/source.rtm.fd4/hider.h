e1d8835abf1b4f79a4018f172803eb4e58c40be5
syauqi <syauqilenterano@gmail.com>
2018-09-23 15:49:04 +0700

    idk

--- C++Programs/source.rtm.fd4/hider.h
+++ C++Programs/source.rtm.fd4/hider.h
@@ -2,27 +2,5 @@
 #define	HIDER
 
 #pragma acc routine seq
-extern double d2xd2y(double *U, double h, int pos, int Nx);
-
-#pragma acc routine seq
-extern double d4(double *U, double *U_, double h, int pos, int dom);
-
-#pragma acc routine seq
-extern double d5(double *U, double *U_, double h, int pos, int dom);
-
-#pragma acc routine seq
-extern double d2x2y(double *U, double *Ux, double *Uy, double h, int pos, int Nx);
-
-#pragma acc routine seq
-extern double dx4y(double *U, double *Uy, double h, int pos, int Nx);
-
-#pragma acc routine seq
-extern double d4xy(double *U, double *Ux, double h, int pos, int Nx);
-
-#pragma acc routine seq
-extern double d3x2y(double *U, double *Ux, double h, int pos, int Nx);
-
-#pragma acc routine seq
-extern double d2x3y(double *U, double *Uy, double h, int pos, int Nx);
-
+extern double d2xd2y(double *U, double h, int pos, int Nx,int Ny);
 #endif
